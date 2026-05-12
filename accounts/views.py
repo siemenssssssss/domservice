217d7507f6327e1240044c3fd24f918fadffad09
siemenssssssss <161227959+siemenssssssss@users.noreply.github.com>
2026-05-12 22:11:49 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -10,12 +10,9 @@ def register(request):
         if form.is_valid():
             user = form.save()
             
-            # Сохраняем адрес дома
             address = form.cleaned_data.get('address')
             if address:
                 house, created = HouseInfo.objects.get_or_create(address=address)
-            else:
-                house = None
             
             Profile.objects.create(
                 user=user,
