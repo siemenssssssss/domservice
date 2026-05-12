cc303ea8b1363aac1faf2ce9c049b20ccb8b43c0
siemenssssssss <161227959+siemenssssssss@users.noreply.github.com>
2026-05-12 19:22:49 +0500

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('my-logout/', views.custom_logout, name='custom_logout'),
-]
