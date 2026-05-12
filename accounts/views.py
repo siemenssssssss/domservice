4253d0a4daeaa3d1fb549ab6b3114851187dd2ac
siemenssssssss <161227959+siemenssssssss@users.noreply.github.com>
2026-05-12 22:07:37 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib import messages
 from main.forms import UserRegisterForm
-from main.models import Profile
+from main.models import Profile, HouseInfo
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
+            
+            # Сохраняем адрес дома
+            address = form.cleaned_data.get('address')
+            if address:
+                house, created = HouseInfo.objects.get_or_create(address=address)
+            else:
+                house = None
+            
             Profile.objects.create(
                 user=user,
                 apartment_number=form.cleaned_data.get('apartment_number'),
