8497ccf06ca58fbef28dcfefdc42491c7fd5bc71
siemenssssssss <161227959+siemenssssssss@users.noreply.github.com>
2026-05-16 01:25:56 +0500

    Delete main directory

--- main/management/commands/check_debts.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from main.utils import check_debt_notifications
-
-class Command(BaseCommand):
-    help = 'Check debts and create notifications'
-
-    def handle(self, *args, **options):
-        check_debt_notifications()
-        self.stdout.write(self.style.SUCCESS('Debt check completed'))
