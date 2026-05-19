d9ea5435f91a1a1c4bca939e16d99cc50121ff10
siemenssssssss <161227959+siemenssssssss@users.noreply.github.com>
2026-05-19 15:54:57 +0500

    Add files via upload

---
+++ main/management/commands/create_default_services.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from main.models import Service
+
+class Command(BaseCommand):
+    help = 'Создает 5 стандартных услуг ЖКХ'
+
+    def handle(self, *args, **kwargs):
+        services_data = [
+            {'name': 'Электроэнергия', 'unit': 'кВт/ч', 'price': 4.50},
+            {'name': 'Холодная вода', 'unit': 'м³', 'price': 35.00},
+            {'name': 'Горячая вода', 'unit': 'м³', 'price': 120.00},
+            {'name': 'Отопление', 'unit': 'Гкал', 'price': 1800.00},
+            {'name': 'Содержание жилья', 'unit': 'м²', 'price': 25.50},
+        ]
+        
+        created_count = 0
+        existing_count = 0
+        
+        for data in services_data:
+            service, created = Service.objects.get_or_create(
+                name=data['name'],
+                defaults={
+                    'unit': data['unit'],
+                    'price': data['price']
+                }
+            )
+            if created:
+                created_count += 1
+                self.stdout.write(self.style.SUCCESS(f'✅ Создана услуга: {service.name} - {service.price} руб/{service.unit}'))
+            else:
+                existing_count += 1
+        
+        self.stdout.write(self.style.SUCCESS(f'\n🎉 ИТОГО: создано {created_count} услуг, уже существовало {existing_count}'))
