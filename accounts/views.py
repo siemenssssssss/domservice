72eda012b83ed9b1773fe8ce47442c9869061929
siemenssssssss <161227959+siemenssssssss@users.noreply.github.com>
2026-06-01 18:17:20 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib import messages
-from main.forms import UserRegisterForm
-from main.models import Profile
+from .forms import UserRegisterForm
+from main.models import Profile, HouseInfo
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
+            selected_house = form.cleaned_data.get('house')
             Profile.objects.create(
                 user=user,
                 apartment_number=form.cleaned_data.get('apartment_number'),
                 phone=form.cleaned_data.get('phone'),
-                personal_account=f"ЛС-{user.id:06d}"
+                personal_account=f"ЛС-{user.id:06d}",
+                house=selected_house
             )
             login(request, user)
             messages.success(request, f'Добро пожаловать, {user.first_name}!')
@@ -21,7 +23,11 @@ def register(request):
     else:
         form = UserRegisterForm()
     
-    return render(request, 'accounts/register.html', {'form': form})
+    houses = HouseInfo.objects.all()
+    return render(request, 'registration/register.html', {
+        'form': form,
+        'houses': houses,
+    })
 
 def custom_logout(request):
     logout(request)
