9f8a476abdc66254b6eebc5f022e903e79482379
siemenssssssss <161227959+siemenssssssss@users.noreply.github.com>
2026-06-07 11:47:56 +0500

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,12 @@ from django.contrib.auth.models import User
 from main.models import HouseInfo
 
 class UserRegisterForm(UserCreationForm):
+    # Личные данные
     first_name = forms.CharField(max_length=30, required=True, label='Имя')
     last_name = forms.CharField(max_length=30, required=True, label='Фамилия')
     email = forms.EmailField(required=True, label='Email')
+    
+    # Адресные данные
     apartment_number = forms.CharField(max_length=10, required=True, label='Номер квартиры')
     phone = forms.CharField(max_length=20, required=True, label='Телефон')
     house = forms.ModelChoiceField(
@@ -17,6 +20,7 @@ class UserRegisterForm(UserCreationForm):
         empty_label='---------'
     )
     
+    # Каптча
     captcha_answer = forms.IntegerField(
         label='Сколько будет 2 + 2?',
         required=True,
@@ -25,7 +29,8 @@ class UserRegisterForm(UserCreationForm):
     
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 
+                  'apartment_number', 'phone', 'house', 'captcha_answer']
     
     def clean_captcha_answer(self):
         answer = self.cleaned_data.get('captcha_answer')
